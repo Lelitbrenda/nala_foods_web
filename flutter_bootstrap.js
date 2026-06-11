53bda8c36dc7647fbbf005c397758dbeafad65ef
Lelitbrenda <Lelitbrenda@users.noreply.github.com>
2026-06-11 09:00:54 +0000

    deploy: c12b007bb2c62144db0c30143bb95745244a594e

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "271128048" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2607289091" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
